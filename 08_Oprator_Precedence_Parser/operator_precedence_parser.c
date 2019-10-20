0c44652deb625049c884be87642d9d39a1489e4b
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-10-20 18:48:42 +0530

    rm comments

--- 08_Oprator_Precedence_Parser/operator_precedence_parser.c
+++ 08_Oprator_Precedence_Parser/operator_precedence_parser.c
@@ -92,39 +92,3 @@ printf("\t\t\t");
 getch();
 }
 
-
-###########################################
-INPUT:
-Enter the no.of terminals:4
-Enter the terminals:* $ + &gt;
-Enter the table values:
-
-OUTPUT:
-Enter the value for * *:&gt;
-Enter the value for * $:&gt;                                                       
-Enter the value for $ i:&lt;                                                       
-Enter the value for $ +:&lt;
-
-Enter the value for $ *:&lt;                                                       
-Enter the value for $ $:accept                                                  
-                                                                                
-**** OPERATOR PRECEDENCE TABLE
-****                                             
-        i       +       *       $                                               
-                                                                                
-i       e       &gt;       &gt;       &gt;                                               
-+       &lt;       &gt;       &lt;       &gt;                                               
-*       &lt;       &gt;       &gt;       &gt;                                               
-$       &lt;       &lt;       &lt;       a
-*/
-Enter the input string:                                                         
-i*i                                                                             
-                                                                                
-STACK                   INPUT STRING                    ACTION                  
-                                                                                
-$                       i*i                     Shift i                         
-$&lt;i                     *i                      Reduce                          
-$                       *i                      Shift *                         
-$&lt;*                     i                       Shift i                         
-$&lt;*&lt;i                                                                           
-String is not accepted
