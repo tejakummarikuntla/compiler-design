62cb364f769ed7fc4c3b0933b5e69a2627c0585f
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-10-20 18:38:02 +0530

    Add rdp

---
+++ 06_Recursive_Decent_Parsing/recursive_decent_parsing.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<conio.h>
+int i=0 ,f=0;
+char str[30];
+void E();
+void Eprime();
+void T();
+void Tprime();
+void F();
+void E()
+{
+printf("\nE->TE'");
+T();
+Eprime();
+}
+void Eprime()
+{
+if(str[i]=='+')
+{
+	printf("\n\E'->+TE'");
+	i++;
+	T();
+	Eprime();
+	}
+else if(str[i]==')')
+	printf("\nE'->^");
+}
+void T()
+{
+printf("\nT->FT'");
+F();
+Tprime();
+}
+void Tprime()
+{
+	if(str[i]=='*')
+		{
+		printf("\nT'->*FT'");
+	i++;
+		F();
+		Tprime();
+		}
+	else if((str[i]==')')||(str[i]=='+'))
+		printf("\nT'->^");
+}
+void F()
+{
+	if(str[i]=='a')
+	{
+		printf("\nF->a");
+		i++;
+	}
+	else if(str[i]=='(')
+	{
+		printf("\nF->(E)");
+		i++;
+		E();
+		if(str[i]==')')
+			i++;
+	}
+	else
+		f=1;
+}
+void main()
+{
+int len;
+clrscr();
+printf("Enter the string: ");
+scanf("%s",str);
+len=strlen(str);
+str[len]='$';
+E();
+if((str[i]=='$')&&(f==0))
+printf("\nString sucessfully parsed!");
+else
+printf("\nSyntax Error!");
+getch();
+}
+
+// Output:
+
+Enter the string: a+a*a$
+E-&gt;TE&#39;
+T-&gt;FT&#39;
+F-&gt;a
+T&#39;-&gt;^
+E&#39;-&gt;+TE&#39;
+T-&gt;FT&#39;
+F-&gt;a
+T&#39;-&gt;*FT&#39;
+F-&gt;a
+String sucessfully parsed!
